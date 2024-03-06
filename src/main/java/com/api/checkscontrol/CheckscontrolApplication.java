ec52689c0782657db1401a8e18d4c88d9ccc29c7
Micael Pereira <mspereira2024@gmail.com>
2024-03-06 18:15:26 +0000

    startup: start checks control project

---
+++ src/main/java/com/api/checkscontrol/CheckscontrolApplication.java
@@ -0,0 +1,13 @@
+package com.api.checkscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckscontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckscontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/checkscontrol/CheckscontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.checkscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckscontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
